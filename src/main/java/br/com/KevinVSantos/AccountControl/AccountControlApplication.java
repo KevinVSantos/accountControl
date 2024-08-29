342a26ff4b3032247260b85b51e75510d90f3316
Kevin Santos <kevinsantos12111@gmail.com>
2024-08-29 19:36:15 -0300

    Initial commit

---
+++ src/main/java/br/com/KevinVSantos/AccountControl/AccountControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.KevinVSantos.AccountControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/KevinVSantos/AccountControl/AccountControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.KevinVSantos.AccountControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
